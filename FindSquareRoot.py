3b723c10954a642088ac5fa5d8bc3acdf70449d2
vaishnavi-soni-1210 <91839698+vaishnavi-soni-1210@users.noreply.github.com>
2022-09-17 14:07:55 +0530

    Add files via upload

--- FindSquareRoot.py
+++ FindSquareRoot.py
@@ -9,22 +9,24 @@ class Solution(object):
             return 0
         if(x == 1):
             return 1
-        if(x == 2 or x == 3):
-            return 1    
         
-        g = x/2;
-        g2 = g + 1;
-        while(g != g2):
-            n = x/ g;
-            g2 = g;
-            g = (g + n)/2;
+        #using binary search, loop from 0 to (x + 1), & sqrRoot * sqrRoot = x
+        left = 0
+        right = x+1
+        mid = 0
+        while(left < right):
+            mid = int((left + (right-1))/2)
 
-        return int(g);
+            if(mid*mid > x): #consider left part of mid
+                right = mid
+            else: #consider right part of mid
+                left = mid + 1
+
+        return left -1 #not returning mid because in between 3 & 4 mid will be 3 & square root can't be 3 so returning previous number from it
 
 obj = Solution()
-#x = 2147395599
-x = 16
+x = 2147395599
+#x = 16
 #x = 4
 sqrRoot = obj.mySqrt(x)
 print(sqrRoot)
-        
