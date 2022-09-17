bd6f109d6e5a63c7587c76f60a4a32fd07a422b1
vaishnavi-soni-1210 <91839698+vaishnavi-soni-1210@users.noreply.github.com>
2022-09-18 02:53:18 +0530

    Add files via upload

---
+++ HouseRobber.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #[rob1,rob2,n,n+1,n+2...]
+        rob1 = 0
+        rob2 = 0
+
+        #loop through all the houses
+        for n in nums:
+            temp = max((rob1+n),rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
+
+obj = Solution()
+nums = [1,2,3,1]
+maxRob = obj.rob(nums)
+print(maxRob)
